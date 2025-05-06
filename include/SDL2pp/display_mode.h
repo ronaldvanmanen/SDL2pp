494f7e4650f1ec5ac35eb4afa02fafeb522361a6
Ronald van Manen <rvanmanen@gmail.com>
2025-05-06 10:05:14 +0200

    Add minimal wrapper for SDL_DisplayMode.

---
+++ include/SDL2pp/display_mode.h
@@ -0,0 +1,48 @@
+// SDL2++
+//
+// Copyright (C) 2025 Ronald van Manen <rvanmanen@gmail.com>
+//
+// This software is provided 'as-is', without any express or implied
+// warranty.  In no event will the authors be held liable for any damages
+// arising from the use of this software.
+// 
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you must not
+//    claim that you wrote the original software. If you use this software
+//    in a product, an acknowledgment in the product documentation would be
+//    appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be
+//    misrepresented as being the original software.
+// 3. This notice may not be removed or altered from any source distribution.
+
+#pragma once
+
+#include <cstdint>
+
+#include <SDL2/SDL_video.h>
+
+#include "pixel_format.h"
+#include "window.h"
+
+namespace sdl2
+{
+    class display_mode
+    {
+    public:
+        display_mode(window & target);
+        
+        pixel_format format() const;
+        
+        std::int32_t width() const;
+
+        std::int32_t height() const;
+
+        std::int32_t refresh_rate() const;
+    
+    private:
+        SDL_DisplayMode _native_handle;
+    };
+}
